b403a1c39733262ccc0254c259aecf1ba54eb19a
Shyfan <410713847@qq.com>
2022-11-23 00:17:22 +0800

    增加生命值属性

--- Source/SkywalkerGameplayAbilitySystem/Private/AttributeSet/SkywalkerAttributeSetBase.cpp
+++ Source/SkywalkerGameplayAbilitySystem/Private/AttributeSet/SkywalkerAttributeSetBase.cpp
@@ -3,3 +3,25 @@
 
 #include "AttributeSet/SkywalkerAttributeSetBase.h"
 
+USkywalkerAttributeSetBase::USkywalkerAttributeSetBase()
+{
+	
+}
+
+void USkywalkerAttributeSetBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(USkywalkerAttributeSetBase, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(USkywalkerAttributeSetBase, MaxHealth, COND_None, REPNOTIFY_Always);
+}
+
+void USkywalkerAttributeSetBase::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USkywalkerAttributeSetBase, Health, OldHealth);
+}
+
+void USkywalkerAttributeSetBase::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(USkywalkerAttributeSetBase, MaxHealth, OldMaxHealth);
+}
--- Source/SkywalkerGameplayAbilitySystem/Public/AttributeSet/SkywalkerAttributeSetBase.h
+++ Source/SkywalkerGameplayAbilitySystem/Public/AttributeSet/SkywalkerAttributeSetBase.h
@@ -4,7 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
+
 #include "SkywalkerAttributeSetBase.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	   GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	   GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	   GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	   GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -13,5 +22,26 @@ UCLASS()
 class SKYWALKERGAMEPLAYABILITYSYSTEM_API USkywalkerAttributeSetBase : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+public:
+	USkywalkerAttributeSetBase();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+#pragma region 生命值
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "SkywalkerGAS", ReplicatedUsing = OnRep_Health)
+		FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(USkywalkerAttributeSetBase, Health);
+
+	UFUNCTION()
+		virtual void OnRep_Health(const FGameplayAttributeData& OldHealth);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "SkywalkerGAS", ReplicatedUsing = OnRep_MaxHealth)
+		FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(USkywalkerAttributeSetBase, MaxHealth);
+
+	UFUNCTION()
+		virtual void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+
+#pragma endregion
 };
