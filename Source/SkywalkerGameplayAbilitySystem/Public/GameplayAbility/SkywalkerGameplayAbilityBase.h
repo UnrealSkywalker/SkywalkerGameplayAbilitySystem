6d028e003bbaa32092d4d4949d70f789154e98bb
Shyfan <410713847@qq.com>
2022-11-27 12:17:16 +0800

    增加基础类

---
+++ Source/SkywalkerGameplayAbilitySystem/Public/GameplayAbility/SkywalkerGameplayAbilityBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "SkywalkerGameplayAbilityBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKYWALKERGAMEPLAYABILITYSYSTEM_API USkywalkerGameplayAbilityBase : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SkywalkerGameplayAbilitySystem/Public/GameplayEffect/SkywalkerGameplayEffectBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "SkywalkerGameplayEffectBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKYWALKERGAMEPLAYABILITYSYSTEM_API USkywalkerGameplayEffectBase : public UGameplayEffect
+{
+	GENERATED_BODY()
+	
+};
