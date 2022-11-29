9b6c1ec57e75a013b65775ec8dc00cf69a2cb3f8
Shyfan <410713847@qq.com>
2022-11-29 17:58:54 +0800

    增加GameplayCue

---
+++ Source/SkywalkerGameplayAbilitySystem/Public/GameplayCue/SkywalkerGCueBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayCueNotify_Actor.h"
+#include "SkywalkerGCueBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKYWALKERGAMEPLAYABILITYSYSTEM_API ASkywalkerGCueBase : public AGameplayCueNotify_Actor
+{
+	GENERATED_BODY()
+	
+};
