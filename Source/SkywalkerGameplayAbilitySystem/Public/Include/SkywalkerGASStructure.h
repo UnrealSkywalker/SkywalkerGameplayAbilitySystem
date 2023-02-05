b6a33c5be26cc5edd20b0be687f938340dc79a33
Shyfan <410713847@qq.com>
2023-02-06 00:16:18 +0800

    保存

--- Source/SkywalkerGameplayAbilitySystem/Public/Include/SkywalkerGASStructure.h
+++ Source/SkywalkerGameplayAbilitySystem/Public/Include/SkywalkerGASStructure.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/DataTable.h"
 
 #include "SkywalkerGASEnum.h"
 
@@ -46,6 +47,71 @@ struct FSkywalkerSkillAttribute
 		return AttributeType == OtherAttributeType;
 	}
 	
+#pragma endregion
+
+};
+
+#pragma endregion
+
+#pragma region 技能配置结构体
+
+// 技能数据结构体
+USTRUCT(BlueprintType)
+struct FSkywalkerSkillDataTable:public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+#pragma region 属性
+		
+	// 技能ID
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Skywalker|SkillData")
+		int32 ID;
+
+	// 技能主类型
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Skywalker|SkillData")
+		ESkywalkerSkillMainType MainType;
+
+	// 技能标签
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Skywalker|SkillData")
+		TSet<ESkywalkerSkillTag> SkillTags;
+
+#pragma endregion
+
+#pragma region 函数
+
+#pragma endregion
+
+};
+
+// 技能等级结构体
+USTRUCT(BlueprintType)
+struct FSkywalkerSkillLevelTable:public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+		
+#pragma region 属性
+
+	// ID，规则：技能ID*1000 + 等级
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Skywalker|SkillLevelData")
+		int32 ID;
+	
+	// 技能属性列表
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Skywalker|SkillLevelData")
+		TArray<FSkywalkerSkillAttribute> SkillAttributes;
+#pragma endregion
+
+#pragma region 函数
+	// 获得技能ID
+	int32 GetSkillID() const
+	{
+		return ID / 1000;
+	}
+
+	// 获得技能等级
+	int32 GetSkillLevel() const
+	{
+		return ID % 1000;
+	}
 #pragma endregion
 
 };
