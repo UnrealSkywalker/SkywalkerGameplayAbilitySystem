a0f34b184731b8209ad275f7552caedb83551c75
Shyfan <410713847@qq.com>
2024-11-11 16:43:37 +0800

    保存

---
+++ Source/SkywalkerGameplayAbilitySystem/Public/AttributeSet/SkywalkerAttributeSet_Physical.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "AttributeSet/SkywalkerAttributeSetMacro.h"
+#include "SkywalkerAttributeSet_Physical.generated.h"
+
+/**
+ * 物理属性集
+ * 类似 物理伤害，护甲这种物理属性
+ */
+UCLASS()
+class SKYWALKERGAMEPLAYABILITYSYSTEM_API USkywalkerAttributeSet_Physical : public UAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * 物理伤害
+	 */
+	UPROPERTY(BlueprintReadOnly, Category = "Attribute")
+	FGameplayAttributeData Damage_Physical;
+	ATTRIBUTE_ACCESSORS(USkywalkerAttributeSet_Physical, Damage_Physical)
+
+	/**
+	 * 护甲
+	 */
+	UPROPERTY(BlueprintReadOnly, Category = "Attribute")
+	FGameplayAttributeData Armor_Physical;
+	ATTRIBUTE_ACCESSORS(USkywalkerAttributeSet_Physical, Armor_Physical)
+
+	/**
+	 * 格挡率
+	 */
+	UPROPERTY(BlueprintReadOnly, Category = "Attribute")
+	FGameplayAttributeData BlockRate_Physical;
+	ATTRIBUTE_ACCESSORS(USkywalkerAttributeSet_Physical, BlockRate_Physical)
+
+	/**
+	 * 格挡效果
+	 */
+	UPROPERTY(BlueprintReadOnly, Category = "Attribute")
+	FGameplayAttributeData BlockEffect_Physical;
+	ATTRIBUTE_ACCESSORS(USkywalkerAttributeSet_Physical, BlockEffect_Physical)
+
+	/**
+	 * 物理暴击率
+	 */
+	UPROPERTY(BlueprintReadOnly, Category = "Attribute")
+	FGameplayAttributeData CritRate_Physical;
+	ATTRIBUTE_ACCESSORS(USkywalkerAttributeSet_Physical, CritRate_Physical)
+
+	/**
+	 * 物理暴击伤害
+	 */
+	UPROPERTY(BlueprintReadOnly, Category = "Attribute")
+	FGameplayAttributeData CritDamage_Physical;
+	ATTRIBUTE_ACCESSORS(USkywalkerAttributeSet_Physical, CritDamage_Physical)
+};
